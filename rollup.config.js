2b43ef22f55e275473ff1af377606c631e555b26
Hiroki Osame <hiroki@weebly.com>
2019-07-08 00:26:14 +0900

    Refactor

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+const babel = require('rollup-plugin-babel');
+const { terser } = require('rollup-plugin-terser');
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+	input: 'src/PseudoWindow.js',
+	plugins: [
+		babel({
+			exclude: 'node_modules/**',
+		}),
+		isProd && terser(),
+	],
+	output: {
+		dir: 'dist',
+		format: 'umd',
+		name: 'PseudoWindow',
+	},
+};
