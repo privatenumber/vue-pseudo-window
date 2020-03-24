e40d065ae036166dd6158b5d72243b7488154d40
Hiroki Osame <hiroki.osame@gmail.com>
2020-03-24 00:23:38 -0400

    Improved compression

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,23 @@
-const babel = require('rollup-plugin-babel');
-const { terser } = require('rollup-plugin-terser');
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import filesize from 'rollup-plugin-filesize';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+export default {
 	input: 'src/pseudo-window.js',
 	plugins: [
 		babel({
 			exclude: 'node_modules/**',
 		}),
 		isProd && terser({
-			// mangle: {
-			// 	properties: {
-			// 		regex: /handlers/
-			// 	}
-			// }
+			mangle: {
+				properties: {
+					regex: /^M_/,
+				},
+			},
 		}),
+		isProd && filesize(),
 	],
 	output: {
 		dir: 'dist',
