c4b6b42c48d6042a1489f58cf45f4b1e67da4da9
Hiroki Osame <osame@squareup.com>
2020-03-26 14:58:11 -0400

    Moved test and added existing check

--- test/pseudo-window.body.spec.js
+++ test/pseudo-window.body.spec.js
@@ -1,7 +1,50 @@
 import { mount } from '@vue/test-utils';
 import PseudoWindow from 'vue-pseudo-window';
 
+describe('Events', () => {
+	it('should catch "click" on body', () => {
+		const clickHandler = jest.fn();
+		mount({
+			template: `
+				<div>
+					<pseudo-window
+						body
+						@click="clickHandler"
+					/>
+				</div>
+			`,
+			components: {
+				PseudoWindow,
+			},
+			methods: {
+				clickHandler,
+			},
+		}, {
+			attachToDocument: true,
+		});
+
+		global.window.document.body.dispatchEvent(new Event('click'));
+		expect(clickHandler).toHaveBeenCalled();
+	});
+});
+
 describe('Class', () => {
+	it('invalid', async () => {
+		mount({
+			template: `
+				<pseudo-window
+					class="static-class"
+				/>
+			`,
+			components: {
+				PseudoWindow,
+			},
+		});
+
+		const { classList } = global.window.document.body;
+		expect(classList.contains('static-class')).toBe(false);
+	});
+
 	it('static class', async () => {
 		const wrapper = mount({
 			template: `
@@ -88,5 +131,29 @@ describe('Class', () => {
 		wrapper.destroy();
 
 		expect(classList.contains('reactive-class')).toBe(false);
+	});
+
+
+	it('work with existing class', async () => {
+		const { classList } = global.window.document.body;
+		classList.add('should-remain');
+
+		const wrapper = mount({
+			template: `
+				<pseudo-window
+					body
+					class="static-class"
+				/>
+			`,
+			components: {
+				PseudoWindow,
+			},
+		});
+
+		expect(classList.contains('static-class')).toBe(true);
+
+		wrapper.destroy();
+
+		expect(classList.contains('should-remain')).toBe(true);
 	});
 });
