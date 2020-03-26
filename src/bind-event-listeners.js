5eeef7954a467a32c760c8a9f7630a4a33aa4fb6
Hiroki Osame <hiroki.osame@gmail.com>
2020-03-25 21:32:08 -0400

    Updated URL to a shorter one

--- src/bind-event-listeners.js
+++ src/bind-event-listeners.js
@@ -1,7 +1,7 @@
 const { hasOwnProperty } = Object.prototype;
 const hasOwn = (obj, prop) => hasOwnProperty.call(obj, prop);
 
-// From: https://github.com/vuejs/vue/blob/6fe07ebf5ab3fea1860c59fe7cdd2ec1b760f9b0/src/core/vdom/helpers/update-listeners.js#L14
+// From: https://github.com/vuejs/vue/blob/b00868c/src/core/vdom/helpers/update-listeners.js#L14
 const normalizeEvent = (M_target, M_name, M_handler) => {
 	const passive = M_name[0] === '&';
 	M_name = passive ? M_name.slice(1) : M_name; // eslint-disable-line no-param-reassign
@@ -46,6 +46,5 @@ const bindEventListners = (element, $listeners) => {
 	}
 	return () => unbindEventListeners(handlers);
 };
-
 
 export default bindEventListners;
